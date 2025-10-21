76914c5c6cca9761c1fb7437fe2e09654c9cb02b
bang izud <haqizud22@gmail.com>
2025-10-21 10:00:52 +0700

    update sourcode

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: '/public/',
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.jsx"], // sesuaikan
             refresh: true,
         }),
+        react(), // kalau pakai React
     ],
+    base: "/", // ← PENTING: jangan '/public/' atau '/build/'
+    build: {
+        outDir: "public/build",
+        manifest: true,
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            },
+        },
+    },
 });
